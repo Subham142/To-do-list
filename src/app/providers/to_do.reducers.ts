3eb540fe9db56b71e7d93b122fc371ed8581e6c1
Subham <subhamy142@gmail.com>
2023-07-28 19:34:26 +0530

    added add new to_do feature

--- src/app/providers/to_do.reducers.ts
+++ src/app/providers/to_do.reducers.ts
@@ -1,29 +1,32 @@
-import { createFeatureSelector, createReducer, createSelector, on } from "@ngrx/store";
-import { TaskModel, tasks } from "./to_do.state";
-import { actions } from "./to_do.actions";
-
+import {
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  on,
+} from '@ngrx/store';
+import { TaskModel, tasks } from './to_do.state';
+import { actions } from './to_do.actions';
 
 export const taskReducer = createReducer(
-    tasks,
-    on(actions.addTaskAction, (state, task) => {
-        return [...state, task];
-    }),
-    on(actions.updateTaskAction, (state, task) => {
-       let tempTaskIndex = state.findIndex((t)=> t.id == task.id);  
-     if(tempTaskIndex !=-1)
-     {
-        state[tempTaskIndex] = task;
-        return [...state];
-     }
-     return [...state];
-    }),
-    on(actions.deleteTaskAction, (state, task) => {
-      let tasks = state.filter((t) => t.id != task.id);
-      return [...tasks];
-     }),
+  tasks,
+  on(actions.addTaskAction, (state, task) => {
+    return [...state, task];
+  }),
+  on(actions.updateTaskAction, (state, task) => {
+    let tempTaskIndex = state.findIndex((t) => t.id == task.id);
+    var tempStates = [...state];
+    if (tempTaskIndex != -1) {
+      tempStates[tempTaskIndex] = task;
+    }
+    return [...tempStates];
+  }),
+  on(actions.deleteTaskAction, (state, task) => {
+    let tasks = state.filter((t) => t.id != task.id);
+    return [...tasks];
+  })
 );
 
-
-export const taskSelector = createSelector(createFeatureSelector("tasks"),
-(tasks:TaskModel[]) => tasks
+export const taskSelector = createSelector(
+  createFeatureSelector('tasks'),
+  (tasks: TaskModel[]) => tasks
 );
--- src/app/to-do-input/to-do-input.component.ts
+++ src/app/to-do-input/to-do-input.component.ts
@@ -1,12 +1,33 @@
-import { Component,Input,OnInit } from '@angular/core';
-import {TaskModel,tasks} from '../providers/to_do.state'
+import { Component, Input, OnInit } from '@angular/core';
+import { TaskModel, tasks } from '../providers/to_do.state';
+import { Store } from '@ngrx/store';
+import { actions } from '../providers/to_do.actions';
+import { taskSelector } from '../providers/to_do.reducers';
+import { state } from '@angular/animations';
 @Component({
   selector: 'app-to-do-input',
   templateUrl: './to-do-input.component.html',
-  styleUrls: ['./to-do-input.component.css']
+  styleUrls: ['./to-do-input.component.css'],
 })
-export class ToDoInputComponent implements OnInit{
+export class ToDoInputComponent implements OnInit {
+  taskInput?: string;
+  tasks?: TaskModel[];
+  constructor(private store: Store) {}
+  
   ngOnInit(): void {
+    this.store.select(taskSelector).subscribe((state) => (this.tasks = state));
+  }
 
+
+  addTask() {
+    if (this.taskInput != '' || this.taskInput != null)
+      this.store.dispatch(
+        actions.addTaskAction({
+          id: this.tasks!.length + 1,
+          completed: false,
+          title: this.taskInput!,
+        })
+      );
+    this.taskInput = '';
   }
 }
