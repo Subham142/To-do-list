3f2eb4ae49b0d0b728066ba159b796c27858c6f2
Subham <subhamy142@gmail.com>
2023-07-29 12:28:26 +0530

    added download CVS feature

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,21 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { TaskModel } from '../providers/to_do.state';
 import { taskSelector } from '../providers/to_do.reducers';
+import { ngxCsv } from 'ngx-csv/ngx-csv';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-
-  tasks:TaskModel[] =[];
-  constructor(private store:Store){}
+  tasks: TaskModel[] = [];
+  constructor(private store: Store) {}
   ngOnInit(): void {
-      this.loadTask();
+    this.loadTask();
   }
-loadTask() {
-  this.store.select(taskSelector).subscribe(
-    (state) => this.tasks= state)
-}
+  loadTask() {
+    this.store.select(taskSelector).subscribe((state) => (this.tasks = state));
+  }
+
+  downloadCsv() {
+    var options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: 'Task Report',
+      useBom: true,
+
+      headers: [
+        'ID',
+        'Title',
+        'Description',
+        'Deadline',
+        'Priority',
+        'Task Taken By',
+        'Status',
+      ],
+    };
+
+    new ngxCsv(this.tasks, 'report', options);
+  }
 }
--- src/app/providers/to_do.state.ts
+++ src/app/providers/to_do.state.ts
@@ -11,23 +11,23 @@ export interface TaskModel {
 export let tasks: TaskModel[] = [
   {
     id: 1,
-    completed: false,
     title: 'hello',
     description: 'polkjdkjhdjd',
     dueDate: '8-9-2023',
     priority: 1,
     status: 1,
     takenBy: 'Mahesh',
+    completed: false,
   },
 
   {
     id: 2,
-    completed: true,
     title: 'yo',
     description: 'uhjd',
     dueDate: '9-9-2023',
     priority: 2,
     status: 2,
     takenBy: 'tunii',
+    completed: true,
   },
 ];
